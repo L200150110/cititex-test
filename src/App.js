b3fccf5cc459bfb97f8a90eddc8815a40ef822b1
Riski Agung <riski97@gmail.com>
2022-02-22 17:38:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.json";
+import React, { useState, useEffect } from "react";
+
+const styles = {
+  table: {
+    width: "100%",
+    border: "1px solid",
+  },
+  th: {
+    backgroundColor: "grey",
+    color: "white",
+  },
+  td: {
+    backgroundColor: "white",
+    color: "black",
+  },
+};
 
 function App() {
+  const [total, setTotal] = useState([]);
+  const [qty, setQty] = useState([]);
+
+  const penjumlahan = (data) => {
+    var hasil = [];
+    data.forEach((item) => {
+      let dataStock = JSON.parse(item["product_stock"]);
+      let hasilSementara = 0;
+      dataStock.forEach((stock) => {
+        hasilSementara += Object.values(stock)[0];
+      });
+      hasil.push(hasilSementara);
+    });
+    return hasil;
+  };
+
+  const persentase = (data) => {
+    var hasil = [];
+    data.forEach((item, i) => {
+      let dataItem = JSON.parse(item["items"]);
+      dataItem.forEach((qty, i) => {
+        if ("qty" in qty) {
+          hasil.push(qty["qty"]);
+        }
+      });
+    });
+    return hasil;
+  };
+
+  useEffect(() => {
+    setTotal(penjumlahan(data["proformaItem"]));
+    setQty(persentase(data["proformaItem"]));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test Cititex</h1>
+        <table style={styles.table}>
+          <tr style={styles.th}>
+            <th>Jakarta-Kedoya</th>
+            <th>Tangerang-Dadap</th>
+            <th>Bekasi-Galaxy</th>
+            <th>Category</th>
+            <th>Product</th>
+            <th>Total Stock</th>
+            <th>Percent %</th>
+            <th>Total Order</th>
+          </tr>
+          {data["proformaItem"].map((item, i) => {
+            return (
+              <tr style={styles.td}>
+                <td>
+                  {JSON.parse(item["product_stock"]).map((pro, i) => {
+                    if ("1" in pro) {
+                      return pro["1"].toLocaleString();
+                    }
+                  })}
+                </td>
+                <td>
+                  {JSON.parse(item["product_stock"]).map((pro, i) => {
+                    if ("3" in pro) {
+                      return pro["3"].toLocaleString();
+                    }
+                  })}
+                </td>
+                <td>
+                  {JSON.parse(item["product_stock"]).map((pro, i) => {
+                    if ("5" in pro) {
+                      return pro["5"].toLocaleString();
+                    }
+                  })}
+                </td>
+                <td>{item["categoryDescription"]}</td>
+                <td>{item["productDescription"]}</td>
+                <td>{total[i].toLocaleString()}</td>
+                <td>{((qty[i] / total[i]) * 100).toFixed(2)} %</td>
+                <td>{qty[i]}</td>
+              </tr>
+            );
+          })}
+        </table>
       </header>
     </div>
   );
